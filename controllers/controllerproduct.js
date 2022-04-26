8c4bb93ccc2667093d163f4f3b1b193af8a59cf1
prateek <stupidcoder22@gmail.com>
2022-04-26 17:13:10 +0530

    pagination api completed

--- controllers/controllerproduct.js
+++ controllers/controllerproduct.js
@@ -31,8 +31,12 @@ const getAllProduct = async (req, res) => {
     const fieldList = field.split(",").join(" ");
     result = result.select(fieldList);
   }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 7;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
   let product = await result;
-  res.status(200).json({ product, nbHits: product.length });
+  res.status(200).json({ nbHits: product.length, product });
 };
 
 module.exports = { getAllProductStatic, getAllProduct };
