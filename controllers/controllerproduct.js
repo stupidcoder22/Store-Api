ec7eabd525970c39f5fbe31c0ea5c3a40f054320
prateek <stupidcoder22@gmail.com>
2022-04-26 16:21:29 +0530

    sort functionality done

--- controllers/controllerproduct.js
+++ controllers/controllerproduct.js
@@ -1,13 +1,31 @@
 const Product = require("../models/productmodel");
 
 const getAllProductStatic = async (req, res) => {
-  throw new Error("testing async error");
-  res.status(200).json({ msg: "product testing route" });
+  const product = await Product.find({}).sort("-price");
+  res.status(200).json({ product, nbHits: product.length });
 };
 
 const getAllProduct = async (req, res) => {
-  await Product.find();
-  res.status(200).json({ msg: "product route" });
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === true ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  let product = await result;
+  res.status(200).json({ product, nbHits: product.length });
 };
 
 module.exports = { getAllProductStatic, getAllProduct };
